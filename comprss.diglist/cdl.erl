3cd42ef726a3a4fdb778054f6152b17613f1f915
Ahmad Elghafari <ahmadelghafari@gmail.com>
2017-08-19 14:12:08 +0200

    fixed the pow return exp value using imported function int_pow published by Bob Ippolito

--- comprss.diglist/cdl.erl
+++ comprss.diglist/cdl.erl
@@ -28,10 +28,13 @@ compress_empty_test() ->
     ?assertEqual(na, run([])).
 
 %%% failing tests
-%compress_uptpPower23Ds_test() ->
-%    ?assertEqual(21916414441094455542526282901, run([219164,14,4,4,1,0,9,4,4,5,5,5,4,2,5,2,6,2,8,2,9,0,1])).
-%compress_4422222lotsofDs_test() ->
-%    ?assertEqual(4409876543444410944555222222222, run([44,0,9,8,7,6,5,4,3,4,44,4,1,0,9,44,555,2,2,2,2,2,2,2,2,2])).
+compress_uptpPower23_14Ds_test() ->
+    ?assertEqual(21916414441094455542526282901, run([219164,14,4,4,1,0,9,4,4,5,5,5,4,2,5,2,6,2,8,2,9,0,1])).
+compress_4422222lotsofDs_test() ->
+    ?assertEqual(4409876543444410944555222222222, run([44,0,9,8,7,6,5,4,3,4,44,4,1,0,9,44,555,2,2,2,2,2,2,2,2,2])).
+
+int_pow_test()->
+    ?assertEqual(100000000000000000000000000, int_pow(10,26)).
 
 run([])->
     na;
@@ -47,6 +50,22 @@ compress([0|T], Acc, N)->
     compress(T, Acc, N + 1);
 compress([H|T], Acc, N) ->
     Nr = trunc(math:log10(H))+1,
-    Zrs = trunc(math:pow(10, N)),
+    %Zrs = trunc(math:pow(10, N)),
+    Zrs = int_pow(10, N),
     compress(T,  Acc + H * Zrs,  Nr + N).
 
+%% Bob Ippolito implemented functino 'int_pow' 
+%% Reference of Source Origin
+%% https://github.com/mochi/mochiweb/blob/master/src/mochinum.erl#L50
+%% http://erlang.org/pipermail/erlang-questions/2012-March/065257.html
+%% The function is copied and utilized here free of use according the terms of the source origin's license
+
+int_pow(_X, 0) ->
+    1;
+int_pow(X, N) when N > 0 ->
+    int_pow(X, N, 1).
+
+int_pow(X, N, R) when N < 2 ->
+    R * X;
+int_pow(X, N, R) ->
+    int_pow(X * X, N bsr 1, case N band 1 of 1 -> R * X; 0 -> R end).
