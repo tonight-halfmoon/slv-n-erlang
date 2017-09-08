0b0f61c9908ad1c89034063818c4e3bf57e17da4
Ahmad Elghafari <ahmadelghafari@gmail.com>
2017-09-08 21:31:26 +0000

    LCM - Least Common Multiple and Jumping Bunnies

---
+++ integer/lcm/jumping.bunnies/jbns.erl
@@ -0,0 +1,39 @@
+-module(jbns).
+-export([main/0, jmp_bns/1]).
+-import(lcm, [lcm/2]).
+-include_lib("eunit/include/eunit.hrl").
+
+%%% Problem Statement: Find out the nearest point where all bunneis can meet while each jumps for a different range.
+
+jmp_bns(L)->
+    jmp_bns(L, 1).
+jmp_bns([], NearestPoint) ->
+    NearestPoint;
+jmp_bns([J1th,J2th|T], 1) ->
+    jmp_bns(T, lcm:lcm(J1th,J2th));
+jmp_bns([Jth|T], LNp) ->
+    jmp_bns(T, lcm:lcm(Jth, LNp)).
+
+main() ->
+    _ = io:fread("", "~d"),
+    Bunnies = read_nbunnies(),
+    io:fwrite("~w~n", [jmp_bns(Bunnies)]),
+    true.
+
+read_nbunnies() ->
+    case io:get_line("") of 
+	eof ->
+	    ok;
+	Line ->
+	  lists:map(fun erlang:list_to_integer/1,string:tokens(Line, "\r\n\t "))
+    end.
+
+
+jmp_bns_test() ->
+    ?assertEqual(12, jmp_bns([2,3,4])).
+
+jmp_bns_5148_test() ->
+    ?assertEqual(5148, jmp_bns([3,4,6,78,99])).
+
+jmp_bns_testcase7_test() ->
+    ?assertEqual(890969009638765049, jmp_bns([997,991,983,977,971,967])).
---
+++ integer/lcm/lcm.erl
@@ -0,0 +1,86 @@
+-module(lcm).
+-export([main/0,lcm/2]).
+-import(gcd, [gcd/2]).
+-include_lib("eunit/include/eunit.hrl").
+
+lcm_1_1_test() ->
+   ?assertEqual(1, lcm(1,1)).
+lcm_0_0_test() -> 
+    ?assertEqual(0, lcm(0,0)).
+lcm_0_1_test() -> 
+    ?assertEqual(0, lcm(0,1)).
+lcm_1_0_test() -> 
+    ?assertEqual(0, lcm(1,0)).
+lcm_7_1_test() -> 
+    ?assertEqual(7, lcm(7,1)).
+lcm_1_7_test() -> 
+    ?assertEqual(7, lcm(1,7)).
+lcm_99_0_test() -> 
+    ?assertEqual(0, lcm(99,0)).
+lcm_0_99_test() -> 
+    ?assertEqual(0, lcm(0,99)).
+lcm_3_2_test() -> 
+    ?assertEqual(6, lcm(3,2)).
+lcm_99_1_test() -> 
+    ?assertEqual(99, lcm(99,1)).
+lcm_1_99_test() -> 
+    ?assertEqual(99, lcm(1,99)).
+lcm_78_7_test() -> 
+    ?assertEqual(546, lcm(78,7)).
+lcm_7_78_test() -> 
+    ?assertEqual(546, lcm(7,78)).
+
+lcm_any_integer_with_zero_test() -> %% ?? why does pass?
+    ?assertEqual(0, lcm(5,0)).
+
+lcm_any_neg_integer_with_zero_test() -> %% ?? why does pass?
+    ?assertEqual(0, lcm(-5,0)).
+
+%%%% ZERO is not a divisor!
+
+jmp_bns_testcase7_test() ->
+    ?assertEqual(988027, lcm(997,991)).
+jmp_bns_testcase71_test() ->
+    ?assertEqual(971230541, lcm(988027,983)).
+jmp_bns_testcase711_test() ->
+    ?assertEqual(971230541, lcm(988027,971230541)).
+jmp_bns_testcase7111_test() ->
+    ?assertEqual(965302379, lcm(988027,977)).
+jmp_bns_testcase71111_test() ->
+    ?assertEqual(937308610009, lcm(965302379,971)).
+jmp_bns_testcase711111_test() ->
+    ?assertEqual(906377425878703, lcm(937308610009,967)).
+
+lcm(1,X)->
+    X;
+lcm(X,1)->
+    X;
+%lcm(0,_) ->
+%    0;
+%lcm(_,0) -> %% this is not absolutely correct; if it is negative then the integer itself, otherwose 0
+%    0;
+%lcm(0,0)->0; %% implicitely defined with two patterns above
+%but it is minus-infinity!
+lcm(0,0) ->
+    'minus-infinity';
+lcm(X,X) ->
+    abs(X);
+lcm(Xsigned,Ysigned) ->
+    X = abs(Xsigned),
+    Y = abs(Ysigned),
+    %io:fwrite("X: ~w; Y: ~w)~n", [X,Y]),
+    %% % div_( mul(X,Y), gcd:gcd(X,Y)).
+    mul(div_(X,gcd:gcd(X,Y)),Y).
+
+mul(X,Y) ->
+    X*Y.
+
+div_(_,0) ->
+    undefined;
+div_(X,Y) ->
+    trunc(X/Y).
+
+main() ->
+    {ok, [X,Y]} = io:fread("Prompt X,Y :> ", "~d~d"),
+    io:fwrite("LCM(~w,~w):= ~w~n", [X,Y, lcm(X,Y)]),
+    true.
