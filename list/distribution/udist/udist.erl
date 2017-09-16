905ab50517eb1c9233b462445edbc834979c4f32
Ahmad Elghafari <ahmadelghafari@gmail.com>
2017-09-16 20:03:40 +0000

    Using STD to distribute.

--- list/distribution/udist/udist.erl
+++ list/distribution/udist/udist.erl
@@ -2,6 +2,15 @@
 -include_lib("eunit/include/eunit.hrl").
 -export([udist/2]).
 
+nor_test() ->
+    ?assertEqual([[1,5,8,7,11],[9,0,8,7,11],[10,11,8,7]], udist([8,7,11],[[1,5],[9,0], [10,11]])).
+
+empty_lsts_test() ->
+    ?assertEqual([[],[]], udist([], [[],[]])).
+
+empty_nor_test() ->
+    ?assertEqual([[1,5],[9,0],[10,11]], udist([],[[1,5],[9,0], [10,11]])).
+
 %%% Types:
 %%% List = [X]
 %%% ListOfLists = [List]
@@ -11,15 +20,46 @@
 
 %%% Returns the same number of lists consumed from input ListOfLists parameter. Each list of ListOfLists will have extra elements according to the said functionality.
 
+%%% How to achieve the same result using
+%%% (1) List Comprehension and Erlang STD
+%%% 10> [[X|Y] || X <- [[1,2,3]], Y <- [[5,7]]].
+%%% [[[1,2,3],5,7]]
+%%% 11> lists:flatten([[[1,2,3], 5,7]]).
+%%% [1,2,3,5,7]
+%%% In order to remove duplicates
+%%% 16> sets:to_list(sets:from_list([1,2,3,5,7,1])).
+%%% [3,2,5,1,7]
+%%% Concrete 
+%%% lists:map(fun(Z) -> lists:flatten(Z) end, [[X|Y] || X <- [[1,2,3]], Y <- [[9,8],[a,b],[x,y]]]).
+%%% [[1,2,3,9,8],[1,2,3,a,b],[1,2,3,x,y]]
+%%% 18> sets:to_list(sets:from_list(lists:flatten([[X|Y] || X <- [[1,2,3]], Y <- [[1,5,7]]]))).
+%%%[3,2,5,1,7]
+%%% 19> In order to remove duplicates in each sub-list
+%%% lists:map(fun(U) -> sets:to_list(sets:from_list(U)) end, lists:map(fun(Z) -> lists:flatten(Z) end, [[X|Y] || X <- [[1,2,3,1]], Y <- [[9,8],[a,b,1],[x,y]]])).
+%%% [[3,9,2,8,1],[3,a,b,2,1],[3,2,x,y,1]]
+%%% Also, it works for input of ListOfLists in two parameters
+%%% lists:map(fun(U) -> sets:to_list(sets:from_list(U)) end, lists:map(fun(Z) -> lists:flatten(Z) end, [[X|Y] || X <- [[1,2,3,1],[o]], Y <- [[9,8],[a,b],[x,y]]])).
+%%% [[3,9,2,8,1],
+%%%  [3,a,b,2,1],
+%%%  [3,2,x,y,1],
+%%%  [9,8,o],
+%%%  [a,b,o],
+%%%  [x,y,o]]
 
-nor_test() ->
-    ?assertEqual([[1,5,8,7,11],[9,0,8,7,11],[10,11,8,7]], udist([8,7,11],[[1,5],[9,0], [10,11]])).
-
-empty_lsts_test() ->
-    ?assertEqual([[],[]], udist([], [[],[]])).
-
-empty_nor_test() ->
-    ?assertEqual([[1,5],[9,0],[10,11]], udist([],[[1,5],[9,0], [10,11]])).
+%%% (2) Using all Erlang STD 
+%%% 2.1 - Map with fold
+%%% 46> lists:flatten(lists:foldl(fun(X, Acc) -> lists:map(fun(Y) -> [Y,X] end,[[5,7]]) end, [], [[1,2,3]])).
+%%% [5,7,1,2,3]
+%%% 2.3. Map with map 
+%%% 52> lists:flatten(lists:map(fun(Y) -> lists:map(fun(X) -> [Y,X] end,[[1,2,3]]) end, [[5,7]])).
+%%% [5,7,1,2,3]
+%%% 53> 
+%%% 50> lists:map( fun(Z) -> lists:flatten(Z)end, lists:map(fun(X) -> lists:map(fun(Y) -> [X,Y] end, [[1,2,3]]) end, [[9,8],[a,b],[x,y]])).
+%%% [[9,8,1,2,3],[a,b,1,2,3],[x,y,1,2,3]]
+%%% 52> lists:map(fun(X) -> lists:map(fun(Y) -> lists:flatten( [X,Y]) end, [[1,2,3], [o]]) end, [[9,8],[a,b],[x,y]]).
+%%% [[[9,8,1,2,3],[9,8,o]],
+%%%  [[a,b,1,2,3],[a,b,o]],
+%%%  [[x,y,1,2,3],[x,y,o]]]
 
 udist(List, ListOfLists) -> 
     udist(List, ListOfLists, []).
