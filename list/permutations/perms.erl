19aa84e8453067e94d6af2cb2bf2d2bb68e59448
Ahmad Elghafari <ahmadelghafari@gmail.com>
2017-09-23 00:56:00 +0000

    Permutations - Generate permutations of a list by implementing Backracking with divide and conquer algorithm - Ouput needs cleanup

--- list/permutations/perms.erl
+++ list/permutations/perms.erl
@@ -1,23 +1,20 @@
 -module(perms).
--export([permute/1]).
+-export([permute/1, swap/1]).
 
 %%% Permutations with Backtracking with divide and conquer algorithm implementation
 
 permute([X,Y]) ->
     [[X,Y], [Y,X]];
 permute(L) ->
-    SwpdLists = swap(L),   
-    [[[HS|Prmtd] || Prmtd <- permute(SL)] || [HS|SL] <- SwpdLists].
+    [[[HS|Prmtd] || Prmtd <- permute(SL)] || [HS|SL] <- swap(L)].
  
 swap(L) ->
     swap(L, length(L), 1, []).
 
-swap([], _, _, SwpdLists) ->
-    SwpdLists;
-swap(L, J, J, SwpdLists) ->
-    [L|SwpdLists];
-swap(L, M, J, SwapdLists) ->
+swap(L, J, J, Slts) ->
+    [L|Slts];
+swap(L, M, J, Slts) ->
     ElemJ = lists:nth(J, L), 
     ElemM = lists:nth(M, L),
     SwpdJ = lists:append([[ElemM], lists:delete(ElemM, lists:delete(ElemJ, L)), [ElemJ]]),
-    swap(L, M -1, J, [SwpdJ|SwapdLists]).
+    swap(L, M -1, J, [SwpdJ|Slts]).
