79c000f3519a6ffc34537420f0e9e45ef9cb95f4
Ahmad Elghafari <ahmadelghafari@gmail.com>
2017-10-05 22:57:10 +0000

    %%% Replaced 'choose:choose(V, 2) * 2' , or 'choose:choose(V, 2) bsl 1' with V*(V-1).

--- list/count/count.pairs/cpdc/cpdc.erl
+++ list/count/count.pairs/cpdc/cpdc.erl
@@ -1,11 +1,13 @@
 -module(cpdc).
 -export([cpdc/1]). 
--import(choose, [choose/2]).
 -include_lib("eunit/include/eunit.hrl"). 
 -include("./include/testcase_input02_02.hrl").
 
 %%% Problem Statement
 %%% Count the total number of pairs of indices (i, j) where Ai = Aj and i =/= j.
+
+%%% Since Choose(N,2) is equivalent to N*(N -1). Then, no need to compute with a time-consuming choose implementation.
+%%% Replaced 'choose:choose(V, 2) * 2' , or 'choose:choose(V, 2) bsl 1' with V*(V-1). 
 
 %%% Solution utilises divide and conquer with spatial counting
 
@@ -16,7 +18,7 @@ cpdc(L) ->
     cpdc(L, maps:new()).
 
 cpdc([], C) ->
-    Fun = fun(_K, V, AccIn) -> case V of 1 -> AccIn; _ -> AccIn + choose:choose(V,2) * 2 end end,
+    Fun = fun(_K, V, AccIn) -> case V of 1 -> AccIn; _ -> AccIn + V * (V - 1) end end,
     maps:fold(Fun, 0, C);
 cpdc([H|T], C) ->
     CurrOcc = maps:get(H, C, 0),
