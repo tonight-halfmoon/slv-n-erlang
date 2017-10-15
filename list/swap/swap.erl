ee0aaa4c260658e7d624c60ff629504d836533c7
Ahmad Elghafari <ahmadelghafari@gmail.com>
2017-10-15 20:43:57 +0000

    still does not halt

--- list/swap/swap.erl
+++ list/swap/swap.erl
@@ -3,8 +3,28 @@
 -include_lib("eunit/include/eunit.hrl").
 -import(dist, [dist/2]).
 
+swap([]) ->
+    [];
+swap([X, Y, Z]) ->
+    [[Y, X, Z], [Z, Y, X], [X, Z, Y]];
+swap([X,Y]) ->
+    [[Y,X], [X,Y]];
+swap([X,Y,Z|[T]]) ->
+    dist(T, swap([X,Y,Z]));
+%swap([X, Y, Z|T]) ->
+%    [[dist(dist(H, P), swap(T)) || P <- swap(T2)] || [H|T2] <- swap([X, Y, Z])];
+swap([X, Y, Z|T]) ->
+    [concat(St, S) || St <- swap(T), S <- swap([X,Y,Z])];
+swap([A]) ->
+    [A].
+
+concat([], L) ->
+    L;
+concat([HoL|ToL], L) ->
+    concat(ToL, [HoL|L]).
+
 swap_test_() ->
-    {"swapping '[1,2,3]' must result in '[1,2,3], [2,1,3] , [3,1,2]'", 
+    {"swapping '[1,2,3]' must result in '[2,1,3], [3,2,1], [1,3,2]'", 
      ?_assertEqual([[2,1,3], [3,2,1], [1,3,2]], swap([1,2,3]))}.
 
 swap_empty_test_() ->
@@ -19,18 +39,15 @@ swap_2500_profile() ->
      {X, _O} = timer:tc(?MODULE, swap, [lists:seq(1, 2500)]), 
      io:fwrite("Profile: ~p microsends~n", [X]).
 
-swap([]) ->
-    [];
-swap([X, Y, Z]) ->
-    [[Y, X, Z], [Z, Y, X], [X, Z, Y]];
-swap([X, Y]) ->
-    [[Y, X], [X, Y]];
-swap([X, Y, Z|T]) ->
-    [[concat(dist(H, P), T) || P <- swap(T2)] || [H|T2] <- swap([X, Y, Z])];
-swap([A]) ->
-    [A].
-
-concat([], L) ->
-    L;
-concat([HoL|ToL], L) ->
-    concat(ToL, [HoL|L]).
+swap_7_elem_list_test_() ->
+    {"swap [1,2,3,4,5,6,7] must halt and equal expected", ?_assertEqual(
+	[[5,6,4,7,2,1,3],
+	 [5,6,4,7,3,2,1],
+	 [5,6,4,7,1,3,2],
+	 [4,5,6,7,2,1,3],
+	 [4,5,6,7,3,2,1],
+	 [4,5,6,7,1,3,2],
+	 [6,4,5,7,2,1,3],
+	 [6,4,5,7,3,2,1],
+	 [6,4,5,7,1,3,2]],
+	swap:swap([1,2,3,4,5,6,7]))}.
