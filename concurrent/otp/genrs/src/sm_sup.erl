c08dcc7871093c43cf24b1fd626191686b7fcde1
Ahmad Elghafari <ahmadelghafari@gmail.com>
2018-01-28 19:58:37 +0000

    AMQP client and RabbitMQ by GenRS

--- concurrent/otp/genrs/src/sm_sup.erl
+++ concurrent/otp/genrs/src/sm_sup.erl
@@ -16,16 +16,16 @@ start_link() ->
 
 init(_Args) ->
     SupFlags = #{strategy => one_for_one, intensity => 1, period => 5},
-    SMchildspecs = #{id => service_manager,
-		     start => {sm, start_link, [#sm_start_args{}]},
-		     shutdown => brutal_kill
-		    },
-    SPchildspecs = #{id => ssp,
-		     start => {sp, start_link, [#sp_start_args{}]},
-		     shutdown => brutal_kill
-		    },
-    AMQP_publisher_childspecs = #{id => amqp_publisher_child,
-				  start => {amqp_pub, start_link, [#amqp_connect_args{exch = ?exch, queue = ?queue}]},
-				  shutdown => brutal_kill
-				 },
-    {ok, {SupFlags, [SMchildspecs, SPchildspecs, AMQP_publisher_childspecs]}}.
+    SM = #{id => service_manager,
+	   start => {sm, start_link, [#sm_start_args{}]},
+	   shutdown => brutal_kill
+	  },
+    SP = #{id => ssp,
+	   start => {sp, start_link, [#sp_start_args{}]},
+	   shutdown => brutal_kill
+	  },
+    AMQPc = #{id => amqp_publisher_child,
+	      start => {amqp_pub, start_link, [#amqp_connect_args{exch = ?exch, queue = ?queue}]},
+	      shutdown => brutal_kill
+	     },
+    {ok, {SupFlags, [SM, SP, AMQPc]}}.
