18113aa6a0ffd48c36e00d580f132df030e028f3
Ahmad Elghafari <ahmadelghafari@gmail.com>
2018-02-24 23:29:26 +0000

    Linked Lists - Utilising Erlang type  to simulate Linked Lists API

--- data.structures/linked.lists/list_lns/src/lnknds.erl
+++ data.structures/linked.lists/list_lns/src/lnknds.erl
@@ -8,15 +8,17 @@
 %%%-------------------------------------------------------------------
 -module(lnknds).
 
--export([new/0,
-	push/2]).
+-export([new/0, new_node/1,
+	 push/2,
+	 from_list/1, to_list/1
+	]).
 
 -export_type([linknodes/0]).
 
--record(time_visited, {timestamp :: erlang:timestmap()}).
+-record(time_created, {timestamp :: erlang:timestmap()}).
 -record(data, {value :: erlang:term()}).
--record(node, {key = 1 :: integer(), data = #data{}, time_visited = #time_visited{}}).
--record(linked_list, {nlist = nolist :: [#node{}] | []}).
+-record(node, {key = 1 :: integer(), data = #data{}, time_created = #time_created{}}).
+-record(linked_list, {list = nolist :: [#node{}] | []}).
 
 -opaque linknodes() :: #linked_list{}.
 
@@ -26,13 +28,22 @@
 
 -spec new() -> linknodes().
 
-new() -> #linked_list{nlist = []}.
+new() -> #linked_list{list = []}.
 
--spec push(LL :: linknodes(), Data :: erlang:term()) -> Result :: linknodes().
+-spec push(Linknodes :: linknodes(), Data :: erlang:term()) -> Result :: linknodes().
 
-push(LL, Data) ->
-    NewNode = #node{key = key_uniform(os:system_time()), data = #data{value = Data}},
-    #linked_list{nlist = [NewNode|LL#linked_list.nlist]}.
+push(Linknodes, Data) ->
+    Linknodes#linked_list{list = push(Linknodes#linked_list.list, Data, fun new_node/1)}.
+
+-spec from_list(erlang:list() | []) -> linknodes().
+
+from_list(L) ->
+    from_list(lists:reverse(L), new()).
+
+-spec to_list(linknodes()) -> erlang:list().
+
+to_list(Linknodes) ->
+    to_list(Linknodes#linked_list.list, []).
 
 %%%===================================================================
 %%% Internal Functions
@@ -46,3 +57,25 @@ key_uniform(State) ->
 	Result ->
 	    Result
     end.
+
+from_list([], Linknodes) ->
+    Linknodes;
+from_list([H|T], Linknodes) ->
+    from_list(T, push(Linknodes, H)).
+
+-spec push(L :: erlang:list() | [], Data :: erlang:term(), NewNodeFun :: fun()) -> erlang:list().
+
+push(L, Data, NewNodeFun) ->
+    [NewNodeFun(Data)|L].
+
+-spec new_node(erlang:term()) -> #node{}.
+
+new_node(Data) ->
+    #node{key = key_uniform(os:system_time()), data = #data{value = Data}}.
+
+-spec to_list(ListOfNodes :: erlang:list(), Result :: erlang:list()) -> Result :: erlang:list().
+
+to_list([], Result) ->
+    lists:reverse(Result);
+to_list(_ListOfNodes = [_H = #node{key = Key, data = #data{value = Data}, time_created = _Tc}|T], Result) ->
+    to_list(T, [{Key, Data}|Result]).
--- data.structures/linked.lists/list_lns/test/lnknds_tests.erl
+++ data.structures/linked.lists/list_lns/test/lnknds_tests.erl
@@ -10,15 +10,17 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
--export([rs/0]).
+-export([eval_testcases/0]).
 
-rs() ->
+-define(module, lnknds).
+
+eval_testcases() ->
     eunit:test([lnknds], [verbose, {report, {eunit_surefire, [{dir, "."}]}}]).
 
 api_new_test_() ->
     {
       "When function `new/0` is invoked, then a new empty Linked List must be returned",
-      ?_assertEqual({linked_list ,[]}, lns:new())
+      ?_assertEqual({linked_list ,[]}, ?module:new())
     }.
 
 api_push_test_() ->
@@ -26,11 +28,59 @@ api_push_test_() ->
       "When function `push/2` is invoked on an empty Linked List and some data input, then it must compose a new node containing the data provided and return a linked list which has the new node",
       {
 	setup,
-	fun() -> lns:new() end,
+	fun() -> ?module:new() end,
 	fun(Linknodes) -> [?_assertMatch({linked_list,[{node,Timestamp,
 					 {data,v1},
-					 {time_visited,undefined}}]} when Timestamp > 0, lns:push(Linknodes, 'v1'))
+					 {time_created,undefined}}]} when Timestamp > 0, ?module:push(Linknodes, 'v1'))
 		   ]
+	end
+      }
+    }.
+
+api_from_list_test_() ->
+    {
+      "When function `from_list/1` is invoked on a non-empty Erlang type `list()`, then it must return a Linked List having the same size of the source list, each node has the data from the next source list's index",
+      {
+	setup,
+	fun() -> [list_to_atom(lists:concat(['v', X])) || X <- lists:seq(1,3)] end,
+	fun(SourceList) -> [?_assertMatch({linked_list, [{node, Ts1,{data, v1}, _Tc1},
+							 {node, Ts2, {data, v2}, _Tc2},
+							 {node, Ts3, {data, v3}, _Tc3}]}
+					  when Ts1 < Ts2; Ts2 < Ts3; Ts1 > 1,
+					       ?module:from_list(SourceList))
+			   ]
+	end
+      }
+    }.
+
+api_from_list_assert_node_creation_timestamp_test_() ->
+    {
+      "When function `from_list/1` is invoked on a non-empty Erlang type `list()`, then it must return a Linked List having the same size of the source list, each node has the data from the next source list's index",
+      {
+	setup,
+	fun() -> SourceList = [list_to_atom(lists:concat(['v', X])) || X <- lists:seq(1,3)],
+		 [?module:from_list(SourceList), os:system_time()] 
+	end,
+	fun([ActualLinkedList, SystemTime]) ->
+		[?_assertMatch({linked_list, [{node, Ts1,{data, v1}, _Tc1},
+					      {node, Ts2, {data, v2}, _Tc2},
+					      {node, Ts3, {data, v3}, _Tc3}]}
+			       when Ts1 < Ts2; Ts2 < Ts3; Ts1 > 1; Ts3 < SystemTime, ActualLinkedList)
+		]
+	end
+      }
+    }.
+
+api_to_list_test_() ->
+    {
+      "When function `to_list/1` is invoked on Linked List, then it must return a Erlang type `list()` having the same size of the source Linked List, each index has a pair of `Key` and `Data` from the next source Linked List's Node",
+      {
+	setup,
+	fun() -> SourceList = [list_to_atom(lists:concat(['v', X])) || X <- lists:seq(1,3)],
+		 ?module:from_list(SourceList)
+	end,
+	fun(SourceLinkedList) ->
+		[?_assertMatch([{_Tc1, v1}, {_Tc2, v2}, {_Tc3, v3}], ?module:to_list(SourceLinkedList))]
 	end
       }
     }.
