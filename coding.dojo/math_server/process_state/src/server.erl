5c4d4ff6cf86a5911e66e9cec0d89cd5505b025f
Ahmad Elghafari <ahmadelghafari@gmail.com>
2018-05-05 07:46:42 +0000

    Math Server - Keep State at Stop

---
+++ coding.dojo/math_server/process_state/src/server.erl
@@ -0,0 +1,61 @@
+-module(server).
+-export([start/0, sum_areas/2, stop/1,
+	 loop/2,
+	 keep_state/3]).
+
+start() ->
+    Pid = spawn(?MODULE, loop, [fun geometry:areas/1, {}]),
+    {ok, Pid}.
+
+sum_areas(Shapes, ServerPid) ->
+    ServerPid ! {request, self(), Shapes},
+    receive
+	{reply, ServerPid, Result} ->
+	    Result
+    end.
+
+stop(ServerPid) ->
+    case not is_process_alive(ServerPid) of
+	true ->
+	    {error, already_stopped};
+	false ->
+	    spawn(?MODULE, keep_state, [self(), ServerPid, {}]),
+	    receive
+		{ok, stopped, State} ->
+		    {ok, stopped, State}
+	    end
+    end.
+
+keep_state(ClientPid, Pid, State) ->
+    process_flag(trap_exit, true),
+    link(Pid),
+    Pid ! {stop, self()},
+    loop_keep_state(ClientPid, Pid, State).
+
+loop_keep_state(ClientPid, Pid, State) ->
+    receive
+	{'EXIT', Pid, _Why} ->
+	    ClientPid ! {ok, stopped, State};
+	{server_state, Pid, NewState} ->
+	   loop_keep_state(ClientPid, Pid, NewState)
+    end.
+
+loop(F, State) ->
+    receive
+	{request, Client, Shapes} ->
+	    Result = eval(F, Shapes),
+	    Client ! {reply, self(), Result},
+	    NewState = {Result, Shapes},
+	    loop(F, NewState);
+	{stop, From} ->
+	    From ! {server_state, self(), State},
+	    exit(normal)
+    end.
+
+eval(F, Shapes) ->
+    case catch F(Shapes) of
+	{'EXIT', Why} ->
+	    {error, Why};
+	Sum ->
+	    {ok, Sum}
+    end.
---
+++ coding.dojo/math_server/process_state/test/server_tests.erl
@@ -0,0 +1,49 @@
+-module(server_tests).
+-include_lib("eunit/include/eunit.hrl").
+-import(server, [start/0, sum_areas/2, stop/1]).
+
+start_test() ->
+    {ok, ServerPid} = start(),
+
+    ?assertMatch(Pid when is_pid(Pid), ServerPid),
+    
+    {ok, stopped, _state} = stop(ServerPid).
+
+sum_areas_test() ->
+    {ok, Pid} = start(),
+    Shapes = [{circle, 3}, {rectangle, 4, 6}],
+
+    Result = sum_areas(Shapes, Pid),
+
+    ?assertEqual({ok, 52.27433388230814}, Result),
+
+    {ok, stopped, _state} = stop(Pid).
+
+stop_test() ->
+    {ok, Pid} = start(),
+
+    {ok, stopped, {}} = stop(Pid),
+
+    ?assertNot(is_process_alive(Pid)),
+    
+    {error, already_stopped} = stop(Pid).
+
+sum_unknown_areas_test() ->
+    {ok, Pid} = start(),
+    Shapes = [{circle, 3, 3}],
+
+    Result = sum_areas(Shapes, Pid),
+
+    ?assertMatch({error, {function_clause, _Detail}}, Result),
+
+    {ok, stopped, _Error} = stop(Pid).
+
+server_send_last_shapes_sum_areas_to_client_when_stop_test() ->
+    Shapes = [{circle, 0.873}, {square, 0.293}],
+    ExpectedState = {{ok, 2.4801478674877355}, Shapes},
+    {ok, Pid} = start(), 
+    _Sum = sum_areas(Shapes, Pid),
+
+    {ok, stopped, State} = stop(Pid),
+
+    ?assertEqual(ExpectedState, State).
