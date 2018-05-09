2125a2e184f4c30dd6656e3b2265345c6fbfedc7
Ahmad Elghafari <ahmadelghafari@gmail.com>
2018-05-09 19:07:34 +0000

    Ring - Central Process

--- concurrent/ring/central-process/test/ring_tests.erl
+++ concurrent/ring/central-process/test/ring_tests.erl
@@ -2,33 +2,15 @@
 -include_lib("eunit/include/eunit.hrl").
 -import(ring, [start/2, spawn_node/0, send_message/3, fetch_message/1]).
 
-start_ring_n_nodes_test() ->
+start_ring_n_nodes_and_send_a_message_around_and_terminate_all_nodes_gracefully_test() ->
     N = 3,
     M = initial_message,
 
     Nodes = start(N, M),
-
-    ?assertEqual(N, length(Nodes)).
-
-send_message_around_the_ring_test() ->
-    Message = initial_message,
-    N = 3,
-    Nodes = start(N, Message),
-    LastNode = lists:last(Nodes),
-    FirstNode = hd(Nodes),
-    SecondNode = lists:nth(2, Nodes),
-    ReplyFromFirstNode = fetch_message(FirstNode),
-    ReplyFromSecondNode = fetch_message(SecondNode),
-    ReplyFromLastNode = fetch_message(LastNode),
-
-    ?assertMatch({Message, SendNode} when SendNode == LastNode, ReplyFromFirstNode),
-    ?assertMatch({Message, SendNode} when SendNode == FirstNode, ReplyFromSecondNode),
-    ?assertMatch({Message, SendNode} when SendNode == SecondNode, ReplyFromLastNode).
-
-when_ring_nodes_receive_a_quit_message_then_they_terminate_gracefully_test() ->
-    Message = initial_message,
-    N = 5,
-    Nodes = start(N, Message),
+    
+    lists:foreach(fun(Next) ->
+			  ?assertMatch({M, _SendNode}, fetch_message(Next))
+		  end, Nodes),
 
     send_message(self(), lists:nth(3, Nodes), quit),
 
@@ -36,7 +18,6 @@ when_ring_nodes_receive_a_quit_message_then_they_terminate_gracefully_test() ->
 		    ok
 	    end,
 
-    lists:foreach(
-      fun(Next) ->
-	      ?assertNot(is_process_alive(Next))
-      end, Nodes).
+    lists:foreach(fun(Next) ->
+			  ?assertNot(is_process_alive(Next))
+		  end, Nodes).
