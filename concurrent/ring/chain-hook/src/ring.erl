4aef78b41aab9dc15f84c309b135c0ecbb19bd48
Ahmad Elghafari <ahmadelghafari@gmail.com>
2018-05-09 21:50:50 +0000

    Ring - Chain Hook

--- concurrent/ring/chain-hook/src/ring.erl
+++ concurrent/ring/chain-hook/src/ring.erl
@@ -1,9 +1,78 @@
 -module(ring).
+-export([start/3, stop/1,
+	 fetch_message/1,
+	 init/0]).
 
--export([start/2, send_message/3, fetch_message/1]).
+start(M, N, Message) ->
+    Nodes = spawn_nodes(N, 0, []),
+    Reordered = [H|_] = lists:reverse(Nodes),
+    send_messages(M, H, Message),
+    {ok, noreply, Reordered}.
 
--include_lib("eunit/include/eunit.hrl").
+stop(First) ->
+    First ! {quit, self()},
+    {ok, noreply}.
 
-start(N, M) ->
-ok.
+fetch_message(Node) ->
+    Node ! {fetch_message, self()},
+    receive
+	M ->
+	    M
+    end.
 
+spawn_nodes(N, N, Nodes) ->
+    Nodes;
+spawn_nodes(N, I, []) ->
+    Node = start_node(),
+    spawn_nodes(N, I + 1, [Node]);
+spawn_nodes(N, I, [H|_T] = Nodes) ->
+    H ! {spawn_next, self()},
+    Next = receive
+	       Pid ->
+		   Pid
+	   end,
+    spawn_nodes(N, I + 1, [Next|Nodes]).
+
+start_node() ->
+    spawn(?MODULE, init, []).
+
+init() ->
+    loop(undefined, []).
+
+loop(Next, Msgs) ->
+    receive
+	{spawn_next, From} ->
+	    Pid = spawn(?MODULE, init, []),
+	    From ! Pid,
+	    loop(Pid, Msgs);
+	{quit, _From} ->
+	    case Next of
+		undefined ->
+		    ok;
+		Pid when is_pid(Pid) ->
+		    Next ! {quit, self},
+		    ok
+	    end;
+	{fetch_message, From} ->
+	    From ! Msgs,
+	    loop(Next, Msgs);
+	{new_message, NewMessage, _From} ->
+	    case Next of
+		undefined ->
+		    loop(Next, [NewMessage|Msgs]);
+		Pid when is_pid(Pid) ->
+		    Next ! {new_message, NewMessage, self()},
+		    loop(Next, [NewMessage|Msgs])
+	    end;
+	_M ->
+	    ok
+    end.
+
+send_messages(M, Node, Message) ->
+    send_message(M, 0, Node, Message).
+
+send_message(M, M, _Node, _Message) ->
+    ok;
+send_message(M, I, Node, Message) ->
+    Node ! {new_message, Message, self()},
+    send_message(M, I + 1, Node, Message).
--- concurrent/ring/chain-hook/test/ring_tests.erl
+++ concurrent/ring/chain-hook/test/ring_tests.erl
@@ -1,6 +1,25 @@
 -module(ring_tests).
 -include_lib("eunit/include/eunit.hrl").
--import(ring, [start/2, spawn_node/0, send_message/3, fetch_message/1]).
+-import(ring, [start/3, stop/1, fetch_message/1]).
 
-start_ring_n_nodes_and_send_a_message_around_and_terminate_all_nodes_gracefully_test() ->
-	ok.
+start_ring_N_nodes_and_send_M_message_around_and_terminate_all_nodes_gracefully_test() ->
+    Message = initial_message,
+    {ok, noreply, Nodes} = start(3, 3, Message),
+
+    receive after 1 ->
+		    ok
+	    end,
+
+    ExpectedMessages = [Message || _X <- lists:seq(1,3)],
+
+    lists:foreach(fun(Next) ->
+			  ?assertEqual(ExpectedMessages, fetch_message(Next)) end, Nodes),
+
+    {ok, noreply} = stop(hd(Nodes)),
+
+    receive after 1 ->
+		    ok
+	    end,
+
+    lists:foreach(fun(Next) ->
+			  ?assertNot(is_process_alive(Next)) end, Nodes).
