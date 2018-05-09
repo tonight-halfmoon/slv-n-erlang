844bc428a1635b347890aa646c5641adb7fb3cd4
Ahmad Elghafari <ahmadelghafari@gmail.com>
2018-05-09 18:52:31 +0000

    Ring - Central-Process

--- concurrent/ring/central-process/src/ring.erl
+++ concurrent/ring/central-process/src/ring.erl
@@ -1,6 +1,6 @@
 -module(ring).
 
--export([start/2, spawn_node/0, send_message/3, fetch_message/1]).
+-export([start/2, send_message/3, fetch_message/1]).
 
 -include_lib("eunit/include/eunit.hrl").
 
@@ -18,24 +18,24 @@ start(0, 0, Nodes, M) ->
     Nodes;
 start(N, I, [], M) ->
     Node = spawn_node(),
-    start(N - 1, I, [Node], M); 
+    start(N - 1, I, [Node], M);
 start(N, I, Nodes = [H|_], M) ->
     Node = spawn_node(),
     send_message(Node, H, M),
     start(N - 1, I, [Node|Nodes], M).
 
-node_proc(State = #state{message = Message, from = SendNode}) ->
+node_loop(State = #state{message = Message, from = PrevNode}) ->
     receive
 	{fetch_message, From} ->
-	    From ! {reply, self(),  {Message, SendNode}},
-	    node_proc(State);
-	{mailbox, From, NewMessage} ->
-	    NewState = #state{message= NewMessage, from = From},
-	    node_proc(NewState);
+	    From ! {reply, self(),  {Message, PrevNode}},
+	    node_loop(State);
+	{new_message, Node, NewMessage} ->
+	    NewState = #state{message= NewMessage, from = Node},
+	    node_loop(NewState);
 	quit ->
-	    Alive = is_pid(SendNode) andalso is_process_alive(SendNode), 
+	    Alive = is_pid(PrevNode) andalso is_process_alive(PrevNode),
 	    if  Alive == true ->
-		    send_message(self(), SendNode, quit),
+		    send_message(self(), PrevNode, quit),
 		    ok;
 		Alive == false ->
 		    ok
@@ -43,12 +43,12 @@ node_proc(State = #state{message = Message, from = SendNode}) ->
     end.
 
 spawn_node() ->
-    spawn(fun() -> node_proc(#state{}) end).
+    spawn(fun() -> node_loop(#state{}) end).
 
 send_message(_SendNode, RecvNode, quit) ->
-    RecvNode ! quit; 
+    RecvNode ! quit;
 send_message(SendNode, RecvNode, Message) ->
-    RecvNode ! {mailbox, SendNode, Message},
+    RecvNode ! {new_message, SendNode, Message},
     ok.
 
 fetch_message(RecPid) ->
--- concurrent/ring/central-process/test/ring_tests.erl
+++ concurrent/ring/central-process/test/ring_tests.erl
@@ -10,30 +10,6 @@ start_ring_n_nodes_test() ->
 
     ?assertEqual(N, length(Nodes)).
 
-spawn_node_test() ->
-    NodePid = spawn_node(),
-
-    ?assertMatch(Pid when is_pid(Pid), NodePid).
-
-node_send_message_to_node_test() ->
-    NodePid = spawn_node(),
-    NodePid2 = spawn_node(),
-    Message = hello,
-
-    send_message(NodePid, NodePid2, Message),
-
-    Reply = fetch_message(NodePid2),
-    ?assertEqual({Message, NodePid}, Reply).
-
-when_node_receive_quit_message_then_the_node_terminate_test() ->
-    NodePid = spawn_node(),
-    NodePid2 = spawn_node(),
-
-    send_message(NodePid, NodePid2, quit),
-
-    receive after 50 -> ok end,
-    ?assertNot(is_process_alive(NodePid2)).
-
 send_message_around_the_ring_test() ->
     Message = initial_message,
     N = 3,
@@ -51,12 +27,16 @@ send_message_around_the_ring_test() ->
 
 when_ring_nodes_receive_a_quit_message_then_they_terminate_gracefully_test() ->
     Message = initial_message,
-    N = 3,
+    N = 5,
     Nodes = start(N, Message),
 
-    send_message(self(), hd(Nodes), quit),
-    receive after 50 -> ok end,
+    send_message(self(), lists:nth(3, Nodes), quit),
 
-    ?assertNot(is_process_alive(hd(Nodes))),
-    ?assertNot(is_process_alive(lists:nth(2, Nodes))),
-    ?assertNot(is_process_alive(lists:last(Nodes))).
+    receive after 50 ->
+		    ok
+	    end,
+
+    lists:foreach(
+      fun(Next) ->
+	      ?assertNot(is_process_alive(Next))
+      end, Nodes).
