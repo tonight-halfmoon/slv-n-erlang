ad66b8de7923000da8b12d4e5ce36b188145ed00
Ahmad Elghafari <ahmadelghafari@gmail.com>
2018-05-10 09:06:19 +0000

    Ring - Chain Hook

--- concurrent/ring/chain-hook/test/ring_tests.erl
+++ concurrent/ring/chain-hook/test/ring_tests.erl
@@ -3,14 +3,16 @@
 -import(ring, [start/3, stop/1, fetch_message/1]).
 
 start_ring_N_nodes_and_send_M_message_around_and_terminate_all_nodes_gracefully_test() ->
+    N = 4,
+    M = 3,
     Message = initial_message,
-    {ok, Nodes} = start(3, 3, Message),
+    {ok, Nodes} = start(M, N, Message),
 
     receive after 1 ->
 		    ok
 	    end,
 
-    ExpectedMessages = [Message || _X <- lists:seq(1,3)],
+    ExpectedMessages = [Message || _X <- lists:seq(1, M)],
 
     lists:foreach(fun(Next) ->
 			  ?assertEqual(ExpectedMessages, fetch_message(Next)) end, Nodes),
