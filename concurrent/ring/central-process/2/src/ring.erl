9d899fdcf7e8b78866a9335a9fe89df13fa59c51
Ahmad Elghafari <ahmadelghafari@gmail.com>
2018-05-10 14:23:02 +0000

    Ring - Central Process M messages

--- concurrent/ring/central-process/2/src/ring.erl
+++ concurrent/ring/central-process/2/src/ring.erl
@@ -26,34 +26,34 @@ spawn_nodes(0, 0, Nodes) ->
 spawn_nodes(N, I, []) ->
     spawn_nodes(N - 1, I, [spawn_node(undefined)]);
 spawn_nodes(N, I, Nodes = [H|_]) ->
-    NextNode = spawn_node(H),
-    spawn_nodes(N -1, I, [NextNode|Nodes]).
+    Next = spawn_node(H),
+    spawn_nodes(N -1, I, [Next|Nodes]).
 
 spawn_node(Args) ->
     spawn(?MODULE, init, [Args]).
 
 init(undefined) ->
-    loop({{head, undefined}, []});
+    loop({undefined, []});
 init(Pid) when is_pid(Pid) ->
-    loop({{link, Pid}, []}).
+    loop({Pid, []}).
 
-loop({{Type, Next} = Node, Messages} = State) ->
+loop({Next, Messages} = State) ->
     receive
-	{new_message, _From, NewMessage} ->
-	    case Node of
-		{head, Next} ->
-		    NewState = {{head, Next}, [NewMessage|Messages]},
-		    loop(NewState);
-		{link, Next} ->
-		    NewState = {{Type, Next}, [NewMessage|Messages]},
-		    Next ! {new_message, self(), NewMessage},
-		    loop(NewState)
-	    end;
+	{send_message, Message} ->
+	    Next ! {new_message, {Message, self()}},
+	    loop(State);
+	{new_message, {Message, Node}} when Node == self() ->
+	    NewState = {Next, [Message|Messages]},
+	    loop(NewState);
+	{new_message, {Message, _Node} = MessageTuple} ->
+	    Next ! {new_message, MessageTuple},
+	    NewState = {Next, [Message|Messages]},
+	    loop(NewState);
 	{fetch_message, From} ->
 	    From ! {reply, Messages},
 	    loop(State);
 	{set_next, NewNext} ->
-	    NewState = {{Type, NewNext}, Messages},
+	    NewState = {NewNext, Messages},
 	    loop(NewState);
 	quit ->
 	    case Next of
@@ -73,5 +73,5 @@ send_messages(M, Node, Message) ->
 send_message(M, M, _Node, _Message) ->
     ok;
 send_message(M, I, Node, Message) ->
-    Node ! {new_message, self(), Message},
+    Node ! {send_message, Message},
     send_message(M, I + 1, Node, Message).
