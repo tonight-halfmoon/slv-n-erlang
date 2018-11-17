46c1b6d2cce13eb13b084e62f97d1f3191bdf5dd
Ahmad Elghafari <ahmadelghafari@gmail.com>
2018-11-18 00:51:05 +0100

    2nd unit test: when number is a multiple of 3, then the program returns Fizz

--- coding.dojo/FizzBuzz/src/fizzbuzz.erl
+++ coding.dojo/FizzBuzz/src/fizzbuzz.erl
@@ -3,4 +3,12 @@
 -export([fb/1]).
 
 fb(N) ->
-    N.
+    case isMultipleOf(N, 3) of
+	true ->
+	    'Fizz';
+	_ ->
+	    N
+    end.
+
+isMultipleOf(N, X) ->
+    0 == N rem X.
--- coding.dojo/FizzBuzz/test/fizzbuzz_tests.erl
+++ coding.dojo/FizzBuzz/test/fizzbuzz_tests.erl
@@ -9,3 +9,9 @@ fb_return_number_test_() ->
       "fb when number is not multiple of 3 or 5, then return number",
       ?_assertEqual(1, fb(1))
     }.
+
+fb_return_fizz_test_() ->
+   {
+     "fb when number is multiple of 3, then return 'Fizz'",
+     ?_assertEqual('Fizz', fb(3))
+   }.
