9b1910c3131bc7ef84cd14bddc00199f0121351a
Ahmad Elghafari <ahmadelghafari@gmail.com>
2018-11-18 01:15:23 +0100

    Refactoring step.

--- coding.dojo/FizzBuzz/test/fizzbuzz_tests.erl
+++ coding.dojo/FizzBuzz/test/fizzbuzz_tests.erl
@@ -10,13 +10,13 @@ fb_return_number_test_() ->
       ?_assertEqual(1, fb(1))
     }.
 
-fb_return_fizz_test_() ->
+fb_return_Fizz_test_() ->
    {
      "fb when number is multiple of 3, then return 'Fizz'",
      ?_assertEqual('Fizz', fb(3))
    }.
 
-fb_return_buzz_test_() ->
+fb_return_Buzz_test_() ->
     {
       "fb when number is multiple of 5, then return 'Buzz'",
       ?_assertEqual('Buzz', fb(5))
